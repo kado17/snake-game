68554c36585aea8bfc303b03c3f6234bea00abf6
s1f102100119 <s1f102100119@iniad.org>
2023-12-30 23:07:59 +0900

    環境構築

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'plugin:import/recommended', 'plugin:import/warnings', 'prettier'],
+  rules: {},
+};
---
+++ .stylelintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ['stylelint-config-recess-order', 'stylelint-config-standard'],
+  rules: {},
+  ignoreFiles: ['**/node_modules/**'],
+};
