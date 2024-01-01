a96ee723b5af1f9c05c9c396974d46be6a75590f
s1f102100119 <s1f102100119@iniad.org>
2024-01-01 20:00:37 +0900

    盤面作成

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
   extends: ['stylelint-config-recess-order', 'stylelint-config-standard'],
-  rules: {},
+  rules: {
+    'selector-class-pattern': ['^([a-z][a-z0-9]*)(__[a-z0-9]+)*$'],
+  },
   ignoreFiles: ['**/node_modules/**'],
 };
